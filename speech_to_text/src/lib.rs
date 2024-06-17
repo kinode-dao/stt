60446ec2c6cde4cfb79bce4a36832eb1ccd901e0
jaxs-ribs <luc.baracat@googlemail.com>
2024-06-17 15:06:52 +0200

    update to 0.8

--- speech_to_text/src/lib.rs
+++ speech_to_text/src/lib.rs
@@ -123,7 +123,7 @@ fn handle_message(state: &mut Option<State>) -> Option<()> {
     Some(())
 }
 
-// call_init!(init);
+call_init!(init);
 fn init(_our: Address) {
     let mut state = State::fetch();
     loop {
